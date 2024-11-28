59ebdc99a81cd859767b9abd286e47e2792bdc54
Dave Pearson <davep@davep.org>
2024-11-28 09:43:47 +0000

    :hammer: Start to turn the main module into something more useful
    Just for testing at the moment, of course.

--- src/braindrop/__main__.py
+++ src/braindrop/__main__.py
@@ -1,2 +1,18 @@
+"""Main entry point for the application."""
+
+from pathlib import Path
+
+##############################################################################
+# Local imports.
+from .raindrop import Raindrop
+
+##############################################################################
+def main() -> None:
+    """Main entry point."""
+    print(Raindrop(Path(".test_token").read_text().strip()))
+
+##############################################################################
 if __name__ == "__main__":
-    print("TODO")
+    main()
+
+### __main__.py ends here
