0f6f5cb05be0ca76287aab8149813b116c28612a
Dave Pearson <davep@davep.org>
2024-11-29 13:35:17 +0000

    :sparkles: Add support for working through the collection levels

--- src/braindrop/app/widgets/groups.py
+++ src/braindrop/app/widgets/groups.py
@@ -14,13 +14,17 @@ from ...raindrop import Collection, Raindrop, User
 class CollectionOption(Option):
     """OptionList option class for holding details of a collection."""
 
-    def __init__(self, collection: Collection) -> None:
+    def __init__(self, collection: Collection, indent: int = 0) -> None:
         """Initialise the object.
 
         Args:
             collection: The collection to display with this option.
         """
-        super().__init__(collection.title, id=f"collection-{collection.identity}")
+        super().__init__(
+            f"{'  ' * indent}{collection.title}", id=f"collection-{collection.identity}"
+        )
+        self.collection = collection
+        """The reference to the collection."""
 
 
 ##############################################################################
@@ -46,6 +50,23 @@ class Groups(OptionList):
         self._api = api
         """A reference to the Raindrop API client object."""
 
+    def _add_children_for(
+        self, parent: int, collections: dict[int, Collection], indent: int = 0
+    ) -> None:
+        """Add child collections for the given collection.
+
+        Args:
+            parent: The ID of the parent collection.
+            collections: The dictionary of known collections.
+            indent: The indent level of the parent.
+        """
+        indent += 1
+        for _, collection in collections.items():
+            if collection.parent == parent:
+                self.add_option(CollectionOption(collection, indent))._add_children_for(
+                    collection.identity, collections, indent
+                )
+
     async def show_for_user(self, user: User) -> None:
         """Show the groups and their content for the given user.
 
@@ -58,9 +79,11 @@ class Groups(OptionList):
         }
         self.clear_options()
         for group in user.groups:
-            self.add_options([Separator(), group.title, Separator()])
+            self.add_options([Separator(), f"[b]{group.title}[/b]", Separator()])
             for collection_id in group.collections:
-                self.add_option(CollectionOption(collections[collection_id]))
+                self.add_option(
+                    CollectionOption(collections[collection_id])
+                )._add_children_for(collection_id, collections)
 
 
 ### groups.py ends here
