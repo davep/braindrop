6405ab1317b212f84d8f6e6b569e83885d9e8e5d
Dave Pearson <davep@davep.org>
2024-11-30 07:46:43 +0000

    :sparkles: Add utility code for finding the token file

---
+++ src/braindrop/app/data/__init__.py
@@ -0,0 +1,12 @@
+"""Provides functions and classes for managing the app's data."""
+
+##############################################################################
+# Local imports.
+from .token import token_file
+
+##############################################################################
+# Exports.
+__all__ = ["token_file"]
+
+
+### __init__.py ends here
