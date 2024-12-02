3407999a1c27427def1603c1f830fff647ea3e5e
Dave Pearson <davep@davep.org>
2024-12-02 09:42:52 +0000

    :sparkles: Add code for loading a Raindrop from JSON data

--- src/braindrop/raindrop/raindrop.py
+++ src/braindrop/raindrop/raindrop.py
@@ -1,10 +1,18 @@
 """Provides a class for holding the content of a Raindrop."""
 
 ##############################################################################
+# Backward compatibility.
+from __future__ import annotations
+
+##############################################################################
 # Python imports.
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Literal, TypeAlias
+from typing import Any, Literal, TypeAlias
+
+##############################################################################
+# Local imports.
+from .parse_time import get_time
 
 ##############################################################################
 RaindropType: TypeAlias = Literal[
@@ -23,6 +31,15 @@ class Media:
     type: RaindropType
     """The type of the media."""
 
+    @staticmethod
+    def from_json(data: dict[str, Any]) -> Media:
+        """Create a `Media` instance from JSON-sourced data.
+
+        Returns:
+            A fresh `Media` instance.
+        """
+        return Media(link=data["link"], type=data["type"])
+
 
 ##############################################################################
 @dataclass
@@ -35,7 +52,7 @@ class Raindrop:
     """The ID of the collection that this raindrop belongs to."""
     cover: str
     """The URL to the cover."""
-    created: datetime
+    created: datetime | None
     """The time when the Raindrop was created."""
     domain: str
     """The domain for a link."""
@@ -43,7 +60,7 @@ class Raindrop:
     """The excerpt for the Raindrop."""
     note: str
     """The note for the Raindrop."""
-    last_update: datetime
+    last_update: datetime | None
     """The time the Raindrop was last updated."""
     link: str
     """The URL of the link for the Raindrop."""
@@ -58,6 +75,30 @@ class Raindrop:
     user: int
     """The ID of the owner of the Raindrop."""
     # TODO: More fields here.
+
+    @staticmethod
+    def from_json(data: dict[str, Any]) -> Raindrop:
+        """Create a `Raindrop` instance from JSON-sourced data.
+
+        Returns:
+            A fresh `Raindrop` instance.
+        """
+        return Raindrop(
+            identity=data["_id"],
+            collection=data["collection"]["$id"],
+            cover=data["cover"],
+            created=get_time(data, "created"),
+            domain=data["domain"],
+            excerpt=data["excerpt"],
+            note=data["note"],
+            last_update=get_time(data, "lastUpdate"),
+            link=data["link"],
+            media=[Media.from_json(media) for media in data["media"]],
+            tags=data["tags"],
+            title=data["title"],
+            type=data["type"],
+            user=data["user"]["$id"],
+        )
 
 
 ### raindrop.py ends here
