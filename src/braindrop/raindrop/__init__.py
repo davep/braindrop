fb5a8b877b6f5b9c03c2be1dc082f8f425a01bb0
Dave Pearson <davep@davep.org>
2024-12-04 11:39:35 +0000

    :card_index: Export the get_time function from the Raindrop code

--- src/braindrop/raindrop/__init__.py
+++ src/braindrop/raindrop/__init__.py
@@ -4,11 +4,12 @@
 # Local imports.
 from .api import API
 from .collection import Collection
+from .parse_time import get_time
 from .raindrop import Raindrop, RaindropType
 from .user import Group, User
 
 ##############################################################################
 # Exports.
-__all__ = ["API", "Collection", "Group", "Raindrop", "RaindropType", "User"]
+__all__ = ["API", "Collection", "Group", "Raindrop", "RaindropType", "User", "get_time"]
 
 ### __init__.py ends here
