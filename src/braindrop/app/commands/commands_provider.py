4c65f79d6b239a813f05e365abc4e6e929e6027c
Dave Pearson <davep@davep.org>
2024-12-23 13:52:07 +0000

    :hammer: Make a base class for command-based command palette hits
    This way I can create other command-oriented providers without having to
    repeat myself so much.

---
+++ src/braindrop/app/commands/commands_provider.py
@@ -0,0 +1,78 @@
+"""Provides a base command-oriented command palette provider class."""
+
+##############################################################################
+# Python imports.
+from abc import abstractmethod
+from functools import partial
+from typing import Iterator, NamedTuple, TypeAlias
+
+##############################################################################
+# Textual imports.
+from textual.command import DiscoveryHit, Hit, Hits, Provider
+from textual.message import Message
+
+
+##############################################################################
+class CommandHit(NamedTuple):
+    """A command hit for use in building a command palette hit."""
+
+    command: str
+    """The command."""
+    description: str
+    """The description of the command."""
+    message: Message
+    """The message to emit when the command is chosen."""
+
+
+##############################################################################
+CommandHits: TypeAlias = Iterator[CommandHit]
+"""The result of looking for commands to make into hits."""
+
+
+##############################################################################
+class CommandsProvider(Provider):
+    """A base class for command-message-oriented command palette commands."""
+
+    @abstractmethod
+    def commands(self) -> CommandHits:
+        """Provide the command data for the command palette.
+
+        Yields:
+            A tuple of the command, the command description and a command message.
+        """
+        raise NotImplemented
+
+    async def discover(self) -> Hits:
+        """Handle a request to discover commands.
+
+        Yields:
+            Command discovery hits for the command palette.
+        """
+        for command, description, message in self.commands():
+            yield DiscoveryHit(
+                command,
+                partial(self.screen._post_message, message),
+                help=description,
+            )
+
+    async def search(self, query: str) -> Hits:
+        """Handle a request to search for commands that match the query.
+
+        Args:
+            query: The query from the user.
+
+        Yields:
+            Command hits for the command palette.
+        """
+        matcher = self.matcher(query)
+        for command, description, message in self.commands():
+            if match := matcher.match(command):
+                yield Hit(
+                    match,
+                    matcher.highlight(command),
+                    partial(self.screen._post_message, message),
+                    help=description,
+                )
+
+
+### commands_provider.py ends here
--- src/braindrop/app/commands/tags.py
+++ src/braindrop/app/commands/tags.py
@@ -1,28 +1,20 @@
 """Tag filtering commands for the command palette."""
 
 ##############################################################################
-# Python imports.
-from functools import partial
-from typing import AsyncIterator
-
-##############################################################################
-# Textual imports.
-from textual.command import DiscoveryHit, Hit, Hits, Provider
-
-##############################################################################
 # Local imports.
 from ..data import Raindrops
 from ..messages import ShowTagged
+from .commands_provider import CommandHit, CommandHits, CommandsProvider
 
 
 ##############################################################################
-class TagCommands(Provider):
+class TagCommands(CommandsProvider):
     """A command palette provider related to tags."""
 
     active_collection: Raindrops = Raindrops()
     """The currently-active collection to get the tags from."""
 
-    async def _tag_commands(self) -> AsyncIterator[tuple[str, str, ShowTagged]]:
+    def commands(self) -> CommandHits:
         """Provide the tag-based command data for the command palette.
 
         Yields:
@@ -34,43 +26,11 @@ class TagCommands(Provider):
             "Also tagged" if self.active_collection.is_filtered else "Tagged"
         )
         for tag in self.active_collection.tags:
-            yield (
+            yield CommandHit(
                 f"{command_prefix} {tag.tag}",
                 f"{help_prefix} to Raindrops tagged with {tag.tag} (narrows down to {tag.count})",
                 ShowTagged(tag.tag),
             )
-
-    async def discover(self) -> Hits:
-        """Handle a request to discover commands.
-
-        Yields:
-            Command discovery hits for the command palette.
-        """
-        async for command, description, message in self._tag_commands():
-            yield DiscoveryHit(
-                command,
-                partial(self.screen._post_message, message),
-                help=description,
-            )
-
-    async def search(self, query: str) -> Hits:
-        """Handle a request to search for commands that match the query.
-
-        Args:
-            query: The query from the user.
-
-        Yields:
-            Command hits for the command palette.
-        """
-        matcher = self.matcher(query)
-        async for command, description, message in self._tag_commands():
-            if match := matcher.match(command):
-                yield Hit(
-                    match,
-                    matcher.highlight(command),
-                    partial(self.screen._post_message, message),
-                    help=description,
-                )
 
 
 ### tags.py ends here
