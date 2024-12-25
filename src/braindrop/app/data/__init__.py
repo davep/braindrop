de30b6bf62d2536bbde38a2dbe04e2267397b7d3
Dave Pearson <davep@davep.org>
2024-12-25 08:36:36 +0000

    :sparkles: Start a configuration loading/saving system

--- src/braindrop/app/data/__init__.py
+++ src/braindrop/app/data/__init__.py
@@ -2,13 +2,21 @@
 
 ##############################################################################
 # Local imports.
+from .config import load_configuration, save_configuration
 from .exit_state import ExitState
 from .local import LocalData, Raindrops
 from .token import token_file
 
 ##############################################################################
 # Exports.
-__all__ = ["ExitState", "LocalData", "Raindrops", "token_file"]
+__all__ = [
+    "ExitState",
+    "load_configuration",
+    "LocalData",
+    "Raindrops",
+    "save_configuration",
+    "token_file",
+]
 
 
 ### __init__.py ends here
