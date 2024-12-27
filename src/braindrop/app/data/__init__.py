fda28ee330f67aabb3f129d0ea9c8b9863228a8b
Dave Pearson <davep@davep.org>
2024-12-27 08:12:13 +0000

    :card_index: Export more symbols from the data module

--- src/braindrop/app/data/__init__.py
+++ src/braindrop/app/data/__init__.py
@@ -2,16 +2,18 @@
 
 ##############################################################################
 # Local imports.
-from .config import load_configuration, save_configuration
+from .config import configuration_file, load_configuration, save_configuration
 from .exit_state import ExitState
-from .local import LocalData, Raindrops
+from .local import LocalData, Raindrops, local_data_file
 from .token import token_file
 
 ##############################################################################
 # Exports.
 __all__ = [
+    "configuration_file",
     "ExitState",
     "load_configuration",
+    "local_data_file",
     "LocalData",
     "Raindrops",
     "save_configuration",
