bdec351cca454194f1ba5ec414051d1470cc8dc2
Dave Pearson <davep@davep.org>
2024-12-28 10:40:10 +0000

    :sparkles: Allow overriding the footer text

--- src/braindrop/app/messages/commands.py
+++ src/braindrop/app/messages/commands.py
@@ -31,6 +31,13 @@ class Command(Message):
         If no `COMMAND` is provided the class name will be used.
     """
 
+    FOOTER_TEXT: str | None = None
+    """The text to show in the footer.
+
+    Notes:
+        If no `FOOTER_TEXT` is provided the `command` will be used.
+    """
+
     BINDING_KEY: str | None = None
     """The binding key for the command."""
 
@@ -89,7 +96,7 @@ class Command(Message):
         return Binding(
             cls.BINDING_KEY,
             action or cls._default_action_name(),
-            description=cls.command(),
+            description=cls.FOOTER_TEXT or cls.command(),
             tooltip=cls.tooltip(),
             show=show,
         )
