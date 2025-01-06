2de7933b1423180794be6f45aa9f3a24d6f7929e
Dave Pearson <davep@davep.org>
2025-01-06 10:28:20 +0000

    :white_check_mark: Add tests relating to Raindrop editing

--- tests/unit/test_raindrop.py
+++ tests/unit/test_raindrop.py
@@ -2,7 +2,7 @@
 
 ##############################################################################
 # Pytest imports.
-from pytest import mark
+from pytest import mark, raises
 
 ##############################################################################
 # Local imports.
@@ -13,6 +13,29 @@ from braindrop.raindrop import Raindrop, Tag
 def test_brand_new_randrop_reports_brand_new() -> None:
     """A brand new Raindrop should report itself as brand new."""
     assert Raindrop().is_brand_new is True
+
+
+##############################################################################
+def test_editing_a_raindrop() -> None:
+    """Test using the edit method to change a value in a Raindrop."""
+    TITLE = "This is a test"
+    raindrop = Raindrop(title=TITLE)
+    assert raindrop.title == TITLE
+    assert raindrop.edit(title="Changed").title != TITLE
+
+
+##############################################################################
+def test_editing_a_raindrop_property_that_does_not_exist() -> None:
+    """Attempting to edit a property that doesn't exist should be an error."""
+    with raises(TypeError):
+        Raindrop().edit(not_a_property=42)
+
+
+##############################################################################
+def test_an_edited_raindrop_should_be_a_different_instance() -> None:
+    """When you edit a Raindrop it should result in a new instance."""
+    raindrop = Raindrop()
+    assert raindrop.edit(title="Changed") is not raindrop
 
 
 ##############################################################################
