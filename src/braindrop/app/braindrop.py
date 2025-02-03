8d127121dc3230a642416b1a4e523aa1878db7c7
Dave Pearson <davep@davep.org>
2025-02-03 09:04:08 +0000

    :hammer: Swap to inheriting from EnhancedApp

--- src/braindrop/app/braindrop.py
+++ src/braindrop/app/braindrop.py
@@ -6,8 +6,11 @@ import os
 
 ##############################################################################
 # Textual imports.
-from textual.app import App, InvalidThemeError
-from textual.binding import Binding
+from textual.app import InvalidThemeError
+
+##############################################################################
+# Textual enhanced imports.
+from textual_enhanced.app import EnhancedApp
 
 ##############################################################################
 # Local imports.
@@ -22,53 +25,8 @@ from .screens import Main, TokenInput
 
 
 ##############################################################################
-class Braindrop(App[ExitState]):
+class Braindrop(EnhancedApp[ExitState]):
     """The Braindrop application class."""
-
-    CSS = """
-    CommandPalette > Vertical {
-        width: 75%; /* Full-width command palette looks like garbage. Fix that. */
-        background: $panel;
-        SearchIcon {
-            display: none;
-        }
-        OptionList {
-            /* Make the scrollbar less gross. */
-            scrollbar-background: $panel;
-            scrollbar-background-hover: $panel;
-            scrollbar-background-active: $panel;
-        }
-    }
-
-    /* Remove cruft from the Header. */
-    Header {
-        /* The header icon is ugly and pointless. Remove it. */
-        HeaderIcon {
-            visibility: hidden;
-        }
-
-        /* The tall version of the header is utterly useless. Nuke that. */
-        &.-tall {
-            height: 1 !important;
-        }
-    }
-
-    /* General style tweaks that affect all widgets. */
-    * {
-        /* Let's make scrollbars a wee bit thinner. */
-        scrollbar-size-vertical: 1;
-    }
-    """
-
-    BINDINGS = [
-        Binding(
-            "ctrl+p, super+x, :",
-            "command_palette",
-            "Commands",
-            show=False,
-            tooltip="Show the command palette",
-        ),
-    ]
 
     COMMANDS = set()
 
